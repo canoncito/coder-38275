552a9930be126d5d6d50c0f097fd3813d56bd834
Manuel Medina <73318740+canoncito@users.noreply.github.com>
2022-08-16 19:15:44 -0300

    Update ecommerce.js

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -29,7 +29,7 @@ function agregarInventario() {
 alert("Mmsystem la tienda Online mas grande de la Argentina")
 
 /* Le pregunta al usuario el nombre del producto y luego gracias a filter lo que hara sera filtrar en el array de productos hasta encontrar el producto que coincida con lo que el usuario escribio */
-pedido = prompt("Que producto te interesa comprar?  \n Los siguientes son los productos con los que contamos \naudifonos \ncomputadora \nmouse \ncamara \nsensor \ndron \nteclado").toUpperCase()
+pedido = prompt("Que producto te interesa comprar?  \n Los siguientes son los productos con los que contamos \n auriculares \n computadora \n mouse \n camara \n parlantes \n monitor \n teclado").toUpperCase()
 let buscar = productoN.filter(el => el.producto.includes(pedido))   
 
 alert("Perfecto, a continuación te diremos cual es el Producto de tu eleccion, Precio y en que tienda fisica puedes encontrar nuestro producto")    
